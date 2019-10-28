dc50a1546b06c9764a9419ccca713ee61f6c3387
sawiresandrea <sawires.andrea@iisgalvanimi.edu.it>
2019-10-28 10:16:25 +0000

    es quasi finito

---
+++ client/client-angular/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit{
+  results: string[];
+  private BASE_URL:string = '/api';
+    // Inject HttpClient into your component or service.
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+      // Make the HTTP request:
+      this.http.get('https://3000-a1aaaf8e-1269-4cc7-b040-a67cff3d83c3.ws-eu0.gitpod.io/api').subscribe(data => {
+        // Read the result field from the JSON response.
+        this.results = data['results'];
+      });
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,44 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const cors = require('cors');
+const file_product = require('./products.json')
+
+const app = express();
+app.use(cors());
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname+'/html/index.html'));
+});
+
+app.get('/sitemap', function (req, res){
+    res.sendFile(path.join(__dirname+'/html/sitemap.html'));
+});
+
+app.get('/about', function (req, res){
+    res.sendFile(path.join(__dirname+'/html/about.html'));
+});
+
+app.get('/api', (req, res) => {
+  var jsonData = {"results": ["Important 1 ","Thing 2",]};
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(jsonData));
+});
+
+app.get('/api/products', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(file_product));
+});
+
+//Get port from environment and store in Express.
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+//Listen on provided port, on all network interfaces.
+app.listen(port,  () => {
+    console.log('Example app listening on port 3000!');
+});
